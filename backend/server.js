51eb51e733f37e0dd484878905c0d206688b489e
Amrapali Dhobe <amrapalidhobe@Amrapalis-MacBook-Air.local>
2025-07-27 20:15:21 +0530

    Fix backend and prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -44,5 +44,6 @@ app.post('/upload', upload.single('audio'), (req, res) => {
     });
   });
 });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(5000, () => console.log('Server running at http://localhost:5000'));
